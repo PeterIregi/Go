a614a08119ee67a625f518fe9a605a31e99d80bb
PeterIregi <iregipeter4@gmail.com>
2026-01-30 04:47:04 +0300

    Day 2

--- Hello/hello.go
+++ Hello/hello.go
@@ -3,7 +3,16 @@ package main
 
 import "fmt"
 
+// lets create the function
+func Swap(fp, sp string) string {
+	return sp + fp
+}
 func main() {
+	//today I am going to try and see if I can swap the words in the greeting using a function i will create called swap
+	// lets test it out
+	fmt.Println(Swap("Hello ", "World! "))
+	//should print out World! Hello
 	//should print out Hello World to the console when I run it
 	fmt.Println("Hello world!")
+	//was sucessfull
 }
