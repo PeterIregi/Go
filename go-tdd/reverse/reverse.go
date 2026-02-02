cc979f1389390af20d204e8d338ff3a7bd8cde0f
PeterIregi <iregipeter4@gmail.com>
2026-02-02 04:35:37 +0300

    day 4 of Tdd

--- go-tdd/reverse/reverse.go
+++ go-tdd/reverse/reverse.go
@@ -1,13 +1,30 @@
 package main
 
 func Reverse(s string) string {
-	result := []rune(s)
 
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+	stringRune := []rune(s)
+	//turns our string into a slice of runes for more precise representation of special characters
+	final := ""
+	//empty string where we will put our output
 
-		result[i], result[j] = result[j], result[i]
+	if len(s) == 0 {
+		final = ""
+	} else {
+		for i := len(stringRune) - 1; i >= 0; i-- {
+			final += string(stringRune[i])
+		}
 	}
-	return string(result)
+
+	return final
 }
 
 //lets see if it works
+//it didn't we try again later
+//welcome back
+//lets try again and see if our test passes or what we could do if it fails again
+//lets modify the reverse function
+//lets see if the test passes
+//we get an error for when the string is empty lets fix that
+//lets try it now
+//the loop parameters were wrong it should be greater than zero not less than my fault since there is no negative index at least not here
+//lets fix it and see
