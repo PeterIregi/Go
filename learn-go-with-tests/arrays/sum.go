09fd215ea947d3cf15fca8f71bd30e90ca8a8803
piregi <iregipeter4@gmail.com>
2026-02-12 17:20:34 +0300

    made more array tests

--- learn-go-with-tests/arrays/sum.go
+++ learn-go-with-tests/arrays/sum.go
@@ -10,6 +10,7 @@ func Sum(nums []int)int{
 	}
 	return result
 }
+
 //lets see
 // it failed 
 //now lets write enough code to make it pass
@@ -19,3 +20,18 @@ func Sum(nums []int)int{
 //lets see if it passes
 //we will continue with more tests later
 // goodbye for now!
+func SumAll(numbersToSum ... []int)[]int {
+	//numbersToSum ... helps us to state that we want a varying ni=umber of arguments we dont know the number that will be given
+	var sums []int
+
+	for _,numbers := range numbersToSum{
+		//lets append the values instread of adding them
+		sums = append(sums,Sum(numbers))
+	}
+	return sums
+}
+// we cannot compare slices  like this  != we will have to use another way to use another method
+//we will use reflect.DeepEqual() 
+//lets see if that solves our issue
+//lets see if it passes
+//lets try again
--- learn-go-with-tests/arrays/sum_test.go
+++ learn-go-with-tests/arrays/sum_test.go
@@ -3,18 +3,17 @@ package arrays
 
 import(
 	"testing"
+	"reflect"
 )
 
 func TestSum(t *testing.T){
 
 	t.Run("collection of five numbers", func(t *testing.T){
-		//lets create an array of size 5 since arrays have to have a defined size
 		numbers := []int{1, 2, 3, 4, 5}
-			//this is an array of intergers with the size of 5
+			
 
 		got := Sum(numbers)
-		want := 15
-	//our function should return an int 
+		want := 15 
 
 		if got != want{
 			t.Errorf("got %d want %d given %v", got, want, numbers)
@@ -32,9 +31,18 @@ func TestSum(t *testing.T){
 	})
 	
 }
-//the test should fail
-//the error is that we cannot use the type []int a slice of int when our
-//function only takes arrays of size five [5]int
-//lets change that so that it doesn't  cause any issues
-//that means in the test and in the function
-//we want the function to take in slices so that we are not held back by the size of the arrays
+//lets continue where we left off
+//lets test a function sumAll that adds a varryoing number of slices and produces the sum of each slice in the form of a slice
+func TestSumAll(t *testing.T){
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+//the test should fail because we don't have a sum all function
+//lets make it
+//now it says we have no return in our function
+//lets fix that
+// see you next time
