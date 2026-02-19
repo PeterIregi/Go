f6c28db1a7aed6930b8bf197b80c9dfc605e5a86
piregi <iregipeter4@gmail.com>
2026-02-19 16:31:09 +0300

    more on structs methods and interfaces

--- learn-go-with-tests/structs_methods_and_interfaces/shapes.go
+++ learn-go-with-tests/structs_methods_and_interfaces/shapes.go
@@ -4,7 +4,7 @@ import(
 	"math"
 )
 
-//this is the syntax for declaring structs
+
 type Rectangle struct {
 	Width float64
 	Height float64
@@ -19,26 +19,14 @@ type Circle struct{
 func (c Circle) Area()float64{
 	return math.Pi * c.Radius *c.Radius
 }
-
-func Perimeter(rectangle Rectangle) float64{
-	//see we are stating rectagle as a type 
-	return 2*(rectangle.Width + rectangle.Height)
-	//to refer to the values in our struct we use the . followed by the value name
-
+//this interface will accept any shape that has the method Area that gives a float64 output
+//that is the two shapes we have currently lets see if this works now
+type Shape interface {
+	Area()float64
 }
 
-//we also stopped using  the .2f in the error state ment coz g //offers the decimal 
-//to the precise decimal as compared to the f and in this case preciseness is what we are after
-//lets see now
-//we still cannot pass circle as aruments in our function
-//the test will fail but for other reasons 
-//we also cannot re declare this function
-//but we can declare it as a method in our structs lets change the test first
-//lets make the necessary changes
-//now it should fail because of the out put 
-//now lets fix the out put 
-//for us to find the area of a circle we need to borrow pi 
-//from the math package 
-//hence we need to import it to use the pi constant
-//it should work now
-//thats all for today
+func Perimeter(rectangle Rectangle) float64{
+	return 2*(rectangle.Width + rectangle.Height)
+
+}
+// lets make some changes to our test file so that we can learn about interfaces
--- learn-go-with-tests/structs_methods_and_interfaces/shapes_test.go
+++ learn-go-with-tests/structs_methods_and_interfaces/shapes_test.go
@@ -1,4 +1,3 @@
-// we will try do deal with structs
 package structs_and_methods
 
 import(
@@ -17,31 +16,29 @@ func TestPerimeter(t *testing.T){
 }
 
 func TestArea(t *testing.T){
+
+	checkArea := func(t *testing.T, shape Shape, want float64){
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
 	t.Run("rectangles", func (t *testing.T){
 		rectangle := Rectangle{12.0, 6.0}
-		got := rectangle.Area()
-		want:=72.0
+		checkArea(t, rectangle, 72.0)
 
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+		
 	})
 	t.Run("circles", func (t *testing.T){
 		circle := Circle{10}
-		got := circle.Area()
-		want := 314.1592653589793
+		checkArea(t, circle, 314.1592653589793)
 
-		if got != want {
-			t.Errorf("got %g want %g ", got, want)
-		}
 	})
 		
 	
 }
-//lets continue
-//we want to build a function that finds the area of a circle
-//should fail coz of some obvious reasons
-//circle is undefined and cannot be passed to our function
-//lets try and fix that
-//now it should fail because we have no method in both rectangle and circle called Area
-//lets see
+//since there is some repeating code here lets try to reduce that
+//lets see what ur error will be
+//it says shape is undefined lets define it then
+//we will do more with structs methods and interfaces later including incoporating them in our tests later
