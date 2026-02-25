92f3599883c22714b4dd79a185be45014f50f7d1
piregi <iregipeter4@gmail.com>
2026-02-25 17:35:50 +0300

    pointers and errors part 5

--- learn-go-with-tests/pointersAndErrors/pointer.go
+++ learn-go-with-tests/pointersAndErrors/pointer.go
@@ -2,16 +2,13 @@ package pointersAndErrors
 
 import(
 	"fmt"
+	"errors"
 )
 
 type Bitcoin int
-//in Go we can create a type from an existing type 
-//now we will use  stringer  so that we can  manipulate how it 
-//displays when we print it out in format strings and use  %s
+
 func (b Bitcoin) String()string{
 	return fmt.Sprintf("%d BTC", b)
-	//we have to import the fmt package in order to use fmt.
-	//now lets modify our tests so that it prints out what we want
 }
 
 type Wallet struct{
@@ -23,16 +20,14 @@ func (w *Wallet) Deposit(amount Bitcoin){
 func (w *Wallet) Balance()Bitcoin{
 	return w.balance
 }
-func (w *Wallet) Withdraw(amount Bitcoin){
-	w.balance-= amount
+func (w *Wallet) Withdraw(amount Bitcoin) error{
+	if amount > w.balance{
+		return errors.New("Oh no!")
+		//this lets us create a new error with a message of our choosing
+	}
+	w.balance -= amount
+	return nil
 }
-
-//now lets see if our wallet could contain bitcoins 
-//since we are currently just using int
-//lets make the test fail so we see if it prints out what we want
-//stringer works
-//what we wanted to withdraw
-//lets make some test for that 
-//lets write enough code so that it compiles
-//lets see if it passes
-//later we deal with a situationwhere the withdrawn amount is larger than the balance 
+//our method for withdraw needs to return an error currently it has no return value
+//we can import the error package and do some more interesting things with our error in terms of the message lets see
+//lets make our test more readable y creating a test helper for our error check
--- learn-go-with-tests/pointersAndErrors/pointers_test.go
+++ learn-go-with-tests/pointersAndErrors/pointers_test.go
@@ -5,35 +5,51 @@ import(
 )
 
 func TestWallet(t *testing.T){
+	assertBalance := func(t testing.TB,wallet Wallet, want Bitcoin){
+		t.Helper()
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
+	assertError := func(t testing.TB, err error){
+		t.Helper()
+		if err == nil {
+			t.Error("Wanted an error but didn't get one")
+		}
+	}
 
 	t.Run("deposit", func (t *testing.T){
 		wallet :=Wallet{}
 
 		wallet.Deposit(Bitcoin(10))
 
-		got := wallet.Balance()
-		want := Bitcoin(10)
+		assertBalance(t, wallet, Bitcoin(10))
 
-		if got  != want{
-			t.Errorf("got %s want %s", got, want)
-		}
 	})
 
 	t.Run("withdraw", func(t *testing.T){
-		wallet := Wallet{balance: Bitcoin(20)}
-
+		wallet :=Wallet{balance:Bitcoin(20)}
 		wallet.Withdraw(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
 
-		got := wallet.Balance()
+	t.Run("Withdraw insufficient funds", func(t *testing.T){
+		startingBalance:= Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
 
-		want := Bitcoin(10)
+		assertBalance(t, wallet, startingBalance)
 
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertError(t, err)
 	})
 }
-
+//lets see how we would deal with the case where we want to withdraw more than waht is in our balance 
+//it should give us an error message right lets try that 
+//lets see what the compiler says 
+//its says our withdraw method returns no value yet we are asking for an error
+//lets fix that
+//lets close it of at that 
 	
-//lets see what the compiler says could be the issue 
-//withdraw is not a method in our program
