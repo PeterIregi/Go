c7622eaffcde5ff2c060d28c5aa3a6000303b403
piregi <iregipeter4@gmail.com>
2026-03-04 16:12:14 +0300

    Maps part 3

--- learn-go-with-tests/Maps/dictionary.go
+++ learn-go-with-tests/Maps/dictionary.go
@@ -6,27 +6,20 @@ import(
 
 
 type Dictionary map[string]string
-//we have declared our type dictionary of map
-//now on to the function
 
-//we would have used pointers if we were actualy changing something in our dictionary but since we are just displaying it 
-//.we will leave it as is
+var ErrorNotFound = errors.New("Could not find the word you were looking for")
+
 func (d Dictionary)Search(word string) (string, error){
 	definition, ok := d[word]
-	//it will give us the value in the key [word] and ok  will be nil 
-	if  !ok {
-		//this says what to do if ok is not nil 
-		return "", errors.New("could not find the word you are looking for")
+	 
+	if  !ok { 
+		return "", ErrorNotFound
 	}
 	
 	return definition, nil
 }
 
-//now what if we wantede to search fo a word that  is not in our dictionary what happens 
-//first the test
-//lets make a test for unknown words or words not in our dictionary first
-//does this solve any thing since now we have two return values for our method
-//lets import the errors package first 
-//lets try it now//
-//the problem was the string we were returning as the error
-//later
+func (d Dictionary) Add(word, definition string){
+	d[word] = definition
+}
+
--- learn-go-with-tests/Maps/dictionary_test.go
+++ learn-go-with-tests/Maps/dictionary_test.go
@@ -9,29 +9,33 @@ func TestSearch(t *testing.T){
 	dictionary := Dictionary{"test": "this is just a test"}
 	t.Run("known word", func (t *testing.T){
 		got, _ := dictionary.Search("test")
-		//the _ means the method should return two things but we only need one so 
-		//_just means ignore the other one and give me the one i have requested
-		//this is because in Go we cannot declare a variable and not use it
+		
 		want := "this is just a test"
 
 		assertStrings(t, got, want)
 	})
-	//now we expect some complaints from the compiler cause we only return one value
-	//from our method
-	//lets just make sure
 	t.Run("unkown word", func (t *testing.T){
-		_, err := dictionary.Search("unknown")
-		//now we have no use for the first value hence the _ at the beginning
-		want := "could not find the word you are looking for"
+		_, got := dictionary.Search("unknown")
 
-		if err == nil{
-			//this just says if there is no error message of which should be wrong
+		if got == nil{
 			t.Fatal("expected to get an error")
 
 		}
-		assertStrings(t, err.Error(), want)
+		assertError(t, got, ErrorNotFound)
 	})
 }
+func TestAdd(t *testing.T){
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
+
+	want := "this is just a test"
+	got , err := dictionary.Search("test")
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+	assertStrings(t, got,want)
+}
+
 func assertStrings (t testing.TB,got, want string){
 	t.Helper()
 
@@ -39,8 +43,12 @@ func assertStrings (t testing.TB,got, want string){
 		t.Errorf("got %q want %q", got, want)
 	}
 }
-//lets continue
-//lets first modify our code so that we reduce repetition in future
-//now lets make a dictionary type to wrap our map and add a search method to it but first the test
-//lets go define dictionary in our dictionary .go
-//lets try to fix that
+
+func assertError(t testing.TB, got , want error){
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
+	}
+} 
+//what we wanted to add something to our dictionary instead of just searching
